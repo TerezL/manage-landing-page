2390ec8a328dbd934a2026c73c2d7a5712d3606a
TerezL <terez14@seznam.cz>
2026-04-20 16:45:16 +0200

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/manage-landing-page/",
   plugins: [
     react(),
     tailwindcss()
